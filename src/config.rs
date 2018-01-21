3fe2501bd76907afdbfaf2d97e60e5f7374fdcea
François Mockers <mockersf@gmail.com>
2018-01-21 15:49:56 +0100

    moving configuration

---
+++ src/config.rs
@@ -0,0 +1,67 @@
+use clap::{App, Arg};
+
+#[derive(Debug, Clone)]
+pub struct Config {
+    pub port: u16,
+    pub db_nb_connection: usize,
+    pub db_url: String,
+}
+impl Config {
+    pub fn load() -> Config {
+        let version: String = format!("v{}", ::build_info::BUILD_INFO.version);
+
+        // configuration
+        let matches = App::new("i'Krelln")
+            .version(version.as_str())
+            .about("Start i'Krelln server")
+            .arg(
+                Arg::with_name("port")
+                    .short("p")
+                    .long("port")
+                    .takes_value(true)
+                    .value_name("PORT")
+                    .default_value("8080")
+                    .env("PORT")
+                    .help("Listen to the specified port"),
+            )
+            .arg(
+                Arg::with_name("nb_connection")
+                    .long("nb-connection")
+                    .takes_value(true)
+                    .value_name("NB_CONNECTION")
+                    .default_value("5")
+                    .env("NB_CONNECTION")
+                    .help("Open this number of connections to the DB"),
+            )
+            .arg(
+                Arg::with_name("database_url")
+                    .long("db-url")
+                    .takes_value(true)
+                    .value_name("DATABASE_URL")
+                    .env("DATABASE_URL")
+                    .help("Url to the DB"),
+            )
+            .get_matches();
+
+        let port = matches
+            .value_of("port")
+            .and_then(|it| it.parse::<u16>().ok())
+            .unwrap();
+
+        let db_nb_connection = matches
+            .value_of("nb_connection")
+            .and_then(|it| it.parse::<usize>().ok())
+            .unwrap();
+
+        let db_url = matches
+            .value_of("database_url")
+            .expect("missing DATABASE_URL parameter")
+            .to_string();
+
+        Config {
+            port: port,
+            db_nb_connection: db_nb_connection,
+            db_url: db_url,
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -23,54 +23,18 @@ extern crate futures;
 #[macro_use]
 extern crate diesel;
 
-use clap::{App, Arg};
-
 use actix::Actor;
 
 mod build_info;
+mod config;
 mod engine;
 mod http;
 mod db;
 
 fn main() {
-    let version: String = format!("v{}", build_info::BUILD_INFO.version);
-
-    // configuration
-    let matches = App::new("Krelln")
-        .version(version.as_str())
-        .about("Start Krelln server")
-        .arg(
-            Arg::with_name("port")
-                .short("p")
-                .long("port")
-                .takes_value(true)
-                .value_name("PORT")
-                .default_value("8080")
-                .env("PORT")
-                .help("Listen to the specified port"),
-        )
-        .arg(
-            Arg::with_name("nb_connection")
-                .long("nb-connection")
-                .takes_value(true)
-                .value_name("NB_CONNECTION")
-                .default_value("5")
-                .env("NB_CONNECTION")
-                .help("Open this number of connections to the DB"),
-        )
-        .arg(
-            Arg::with_name("database_url")
-                .long("db-url")
-                .takes_value(true)
-                .value_name("DATABASE_URL")
-                .env("DATABASE_URL")
-                .help("Url to the DB"),
-        )
-        .get_matches();
-
     // log set up
     fern::Dispatch::new()
-        .level(log::LogLevelFilter::Trace)
+        .level(log::LogLevelFilter::Info)
         .level_for("krelln", log::LogLevelFilter::Trace)
         .level_for("tokio_core", log::LogLevelFilter::Error)
         .level_for("mio", log::LogLevelFilter::Error)
@@ -87,28 +51,18 @@ fn main() {
         .apply()
         .unwrap();
 
-    let port = matches
-        .value_of("port")
-        .and_then(|it| it.parse::<u16>().ok())
-        .unwrap();
+    let config = config::Config::load();
 
-    let db_nb_connection = matches
-        .value_of("nb_connection")
-        .and_then(|it| it.parse::<usize>().ok())
-        .unwrap();
-
-    let db_url = matches
-        .value_of("database_url")
-        .expect("missing DATABASE_URL parameter")
-        .to_string();
-
-    info!("Hello, world!");
+    info!("Starting i'Krelln with config: {:?}", config);
 
     let system = actix::System::new("i'krelln");
-    let db_actor = actix::SyncArbiter::start(db_nb_connection, move || {
-        db::DbExecutor(db::establish_connection(db_url.clone()))
-    });
+    let db_actor = {
+        let db_url = config.db_url;
+        actix::SyncArbiter::start(config.db_nb_connection, move || {
+            db::DbExecutor(db::establish_connection(db_url.clone()))
+        })
+    };
     let ingestor_actor: actix::SyncAddress<_> = engine::ingestor::Ingestor(db_actor).start();
-    http::serve(port, ingestor_actor);
+    http::serve(config.port, ingestor_actor);
     system.run();
 }

a90c95bc58b9a8cfc60c34a557963f4899fcaf1d
François Mockers <mockersf@gmail.com>
2018-01-22 23:16:00 +0100

    can use postgres or sqlite based on feature

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -5,13 +5,23 @@ pub mod schema;
 pub mod ingest_event;
 pub mod test_result;
 
+#[cfg(feature = "sqlite")]
 pub fn establish_connection(database_url: String) -> SqliteConnection {
     info!("opening connection to DB {}", database_url);
     SqliteConnection::establish(&database_url)
         .expect(&format!("Error connecting to {}", database_url))
 }
-
+#[cfg(feature = "sqlite")]
 pub struct DbExecutor(pub SqliteConnection);
+
+#[cfg(feature = "postgres")]
+pub fn establish_connection(database_url: String) -> PgConnection {
+    info!("opening connection to DB {}", database_url);
+    PgConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
+}
+#[cfg(feature = "postgres")]
+pub struct DbExecutor(pub PgConnection);
+
 
 impl Actor for DbExecutor {
     type Context = SyncContext<Self>;

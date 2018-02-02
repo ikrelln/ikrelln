136d21b041f57ac783344403beeb53aaacefb8d6
François Mockers <mockersf@gmail.com>
2018-02-02 22:54:42 +0100

    cleaner pipkin ui config

--- src/http/healthcheck.rs
+++ src/http/healthcheck.rs
@@ -29,9 +29,39 @@ pub fn healthcheck(req: HttpRequest<AppState>) -> HttpResponse {
         .unwrap()
 }
 
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+struct ZipkinUiConfig {
+    environment: String,
+    query_limit: u16,
+    default_lookback: u32,
+    instrumented: String,
+    logs_url: Option<String>,
+    search_enabled: bool,
+    dependency: DependencyErrorRates,
+}
+
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+struct DependencyErrorRates {
+    low_error_rate: f32,
+    high_error_rate: f32,
+}
+
 pub fn zipkin_ui_config(_: HttpRequest<AppState>) -> HttpResponse {
     httpcodes::HTTPOk
-    .build()
-    .content_type("application/application/json")
-    .body(r#"{"environment":"","queryLimit":10,"defaultLookback":3600000,"instrumented":".*","logsUrl":null,"searchEnabled":true,"dependency":{"lowErrorRate":0.5,"highErrorRate":0.75}}"#).unwrap()
+        .build()
+        .json(ZipkinUiConfig {
+            environment: "".to_string(),
+            query_limit: 10000,
+            default_lookback: 3600000,
+            instrumented: ".*".to_string(),
+            logs_url: None,
+            search_enabled: true,
+            dependency: DependencyErrorRates {
+                low_error_rate: 0.5,
+                high_error_rate: 0.75,
+            },
+        })
+        .unwrap()
 }

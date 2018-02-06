599768c42e95ba5a563f4dd2f0494e65dc4f23b6
François Mockers <mockersf@gmail.com>
2018-02-06 20:03:35 +0100

    default config for all-in-one image

--- Dockerfile.all-in-one/start.sh
+++ Dockerfile.all-in-one/start.sh
@@ -3,4 +3,5 @@ sleep 5
 envsubst '\$ZIPKIN_BASE_URL' < /etc/nginx/conf.d/zipkin.conf.template > /etc/nginx/nginx.conf
 nginx &
 /usr/local/bin/diesel setup
-/usr/local/bin/ikrelln
+/usr/local/bin/ikrelln --host 0.0.0.0 --port 7878
+

3590e16f8bfc04e596aaf8949bd0618443946168
François Mockers <mockersf@gmail.com>
2018-03-18 17:06:45 +0100

    move test helper in subdirectory

---
+++ tests/helpers/mod.rs
@@ -0,0 +1,46 @@
+extern crate actix_web;
+extern crate chrono;
+extern crate fern;
+extern crate log;
+extern crate serde_json;
+
+extern crate ikrelln;
+
+use std as TheStd;
+
+use actix_web::test::TestServer;
+
+use ikrelln::api::http_application;
+
+pub const DELAY_SPAN_SAVED_MILLISECONDS: u64 = 200;
+pub const DELAY_RESULT_SAVED_MILLISECONDS: u64 = 200;
+pub const DELAY_REPORT_SAVED_MILLISECONDS: u64 = 300;
+pub const DELAY_SCRIPT_SAVED_MILLISECONDS: u64 = 100;
+pub const DELAY_FINISH: u64 = 500;
+
+pub fn setup_server() -> TestServer {
+    TestServer::with_factory(http_application)
+}
+
+static mut LOGGER_SET_UP: bool = false;
+pub fn setup_logger() {
+    if unsafe { !LOGGER_SET_UP } {
+        unsafe { LOGGER_SET_UP = true };
+        fern::Dispatch::new()
+            .format(|out, message, record| {
+                out.finish(format_args!(
+                    "{}[{}][{}] {}",
+                    chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
+                    record.target(),
+                    record.level(),
+                    message
+                ))
+            })
+            .level(log::LevelFilter::Debug)
+            .level_for("tokio_core", log::LevelFilter::Error)
+            .level_for("tokio_reactor", log::LevelFilter::Error)
+            .chain(TheStd::io::stdout())
+            .apply()
+            .unwrap();
+    }
+}
